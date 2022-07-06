8946be74419cbfd9f79bdaba97d3f80aff48fec5
Polycarp Atalor <polycarpatalor@gmail.com>
2022-07-06 13:11:53 +0100

    PlayerMovement and sounds

---
+++ Desert Boarding/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public int runSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+         rigidbody2d.AddForce(transform.right * runSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+    }
+}
