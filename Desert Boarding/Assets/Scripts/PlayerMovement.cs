d0437c536e5c5c9a455e170afa23ab5b331ceec3
leonadX <achimugu24@gmail.com>
2022-07-08 15:02:02 +0100

    Polishing
    UI implementation
    Game Play Improvement
    Background

--- Desert Boarding/Assets/Scripts/PlayerMovement.cs
+++ Desert Boarding/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,11 +9,18 @@ public class PlayerMovement : MonoBehaviour
     //public GameObject currentRoad;
     public Rigidbody2D rigidbody2d;
 
-
+    public LayerMask Track;
+    public TextMeshProUGUI IncreaseScore;
+    public Transform floorPoint;
+    public Transform deathPoint;
+    public float floorCheckRadius;
     public int runSpeed;
+    public int angleTurned;
     public float speed;
     public static Vector3 currentTrackPosition;
     private bool didTouchMove;
+    public bool isGrounded;
+    public bool possibleFlip;
     private float touchLength;
     private float touchBeginTime;
     private float tapTimeLimit = 0.2f;
@@ -22,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        angleTurned = 0;
         currentTrackPosition = new Vector3(-1, -1, -1);
         didTouchMove = false;
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -46,56 +55,103 @@ public class PlayerMovement : MonoBehaviour
                 {
                     didTouchMove = true;
                 }
-                if (Time.time - touchBeginTime >= 1.0f)
+                if (Time.time - touchBeginTime >= 0.3f && !isGrounded)
                 {
+                    angleTurned -= 5;
                     transform.Rotate(0, 0, -5);
                     //rigidbody2d.AddForce(transform.right * runSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
                 }
                 if (Input.GetTouch(0).phase == TouchPhase.Ended)
                 {
+                    angleTurned = 0;
                     touchLength = Time.time - touchBeginTime;
-                    if (touchLength <= tapTimeLimit && !didTouchMove)
+                    if (touchLength <= tapTimeLimit && !didTouchMove && isGrounded)
+                    {
                         rigidbody2d.AddForce(Vector2.up * speed, ForceMode2D.Force);
-                    audioSource.PlayOneShot(jumpSound);
+                        audioSource.PlayOneShot(jumpSound);
+                    }
                 }
             }
 
-            if (GameManager.Instance.FuelGuage.value - 0.01f > 0)
+            if (GameManager.Instance.FuelGuage.value - 0.01f > 0 && isGrounded)
                 GameManager.Instance.FuelGuage.value -= 0.01f;
-            else
+            else if (isGrounded)
             {
                 GameManager.Instance.FuelGuage.value = 0;
-                GameManager.Instance.isGameOver = true;
                 GameManager.Instance.GameOver();
             }
 
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
             {
                 rigidbody2d.AddForce(Vector2.up * speed, ForceMode2D.Force);
                 audioSource.PlayOneShot(jumpSound);
                 //rb.velocity += Vector2.up * speed;
             }
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.RightArrow) && !isGrounded)
+            {
+                angleTurned -= 5;
                 transform.Rotate(0, 0, -5);
+            }
+
+            if (!Physics2D.OverlapCircle(floorPoint.position, floorCheckRadius, Track))
+                isGrounded = false;
+
+            if (Physics2D.OverlapCircle(deathPoint.position, floorCheckRadius, Track))
+                GameManager.Instance.GameOver();
+
+            if (angleTurned <= -180)
+                possibleFlip = true;
         }
+    }
+
+    public IEnumerator IncrementScore()
+    {
+        IncreaseScore.transform.localScale = new Vector3(2, 0, 1);
+        IncreaseScore.gameObject.SetActive(true);
+        //Debug.Log(IncreaseScore.transform.localScale);
+        while(IncreaseScore.transform.localScale.y < 2)
+        {
+            yield return new WaitForSeconds(0.04f);
+            IncreaseScore.transform.localScale = new Vector3(IncreaseScore.transform.localScale.x, 
+                IncreaseScore.transform.localScale.y + 0.2f, IncreaseScore.transform.localScale.z);
+        }
+        GameManager.Instance.addToScore += 100;
+        yield return new WaitForSeconds(0.5f);
+        IncreaseScore.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.collider.CompareTag("track"))
+        {
+            angleTurned = 0;
+            if (possibleFlip)
+                StartCoroutine(IncrementScore());
+            isGrounded = true;
+            possibleFlip = false;
+        }
+
         //spawning hills
         if(collision.collider.CompareTag("track") && collision.gameObject.transform.position != currentTrackPosition)
         {
+            //isGrounded = true;
             currentTrackPosition = collision.gameObject.transform.position;
             float x = collision.gameObject.transform.GetChild(0).transform.position.x;
             float y = collision.gameObject.transform.GetChild(0).transform.position.y;
             Vector3 target = new Vector3(x + 6, y - 4, 0);
             Instantiate(Roads[Random.Range(0, Roads.Length)], target, Quaternion.identity);
         }
+
+        /*if (collision.collider.CompareTag("death"))
+            GameManager.Instance.GameOver();*/
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (GameManager.Instance.isGameOver)
+            return;
+
         if (collision.CompareTag("fuel"))
         {
             if (GameManager.Instance.FuelGuage.value + 2 <= 10)
