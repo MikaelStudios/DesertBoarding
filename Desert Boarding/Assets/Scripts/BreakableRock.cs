472c1ca5b3caf7596533c05338404b2307f3c893
Polycarp Atalor <polycarpatalor@gmail.com>
2022-08-16 16:17:37 +0100

    Obstacles

---
+++ Desert Boarding/Assets/Scripts/BreakableRock.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BreakableRock : MonoBehaviour
+{
+   private ParticleSystem particle;
+   private SpriteRenderer spriteRenderer;
+   private BoxCollider2D boxCollider;
+
+   private void Awake()
+   {
+      particle = GetComponentInChildren<ParticleSystem>();
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      boxCollider = GetComponent<BoxCollider2D>();
+   }
+
+
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+      if (other.collider.gameObject.GetComponent<PlayerMovement>() && 
+      other.contacts[0].normal.x > 0.5f) 
+      {
+          StartCoroutine(Break());
+      }
+   }
+
+   private IEnumerator Break()
+   {
+     particle.Play();
+     spriteRenderer.enabled = false;
+     yield return new WaitForSeconds(particle.main.startLifetime.constantMax);
+     Destroy(gameObject); 
+   }
+}
