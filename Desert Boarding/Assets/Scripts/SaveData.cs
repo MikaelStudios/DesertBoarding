5ed99c607f51d0782db8ba07167b7a0a6c42ccb0
Ysabel Katchy <ykatchy@gmail.com>
2022-09-16 18:52:02 +0100

    Sounds updated

--- Desert Boarding/Assets/Scripts/SaveData.cs
+++ Desert Boarding/Assets/Scripts/SaveData.cs
@@ -9,8 +9,11 @@ public class SaveData : MonoBehaviour
     public static SaveData Instance;
 
     public TextMeshProUGUI myName;
+    public TextMeshProUGUI enterName;
     public int finalScore;
+    public int highScore;
     public int myScore = 1000;
+    public Button submitButton;
 
     private void Awake()
     {
@@ -20,20 +23,27 @@ public class SaveData : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        highScore = PlayerPrefs.GetInt("Best Score", finalScore);
+        Debug.Log(highScore);
+        submitButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Submit();
     }
 
     public void SendScore()
     {
-        
-        if (0 < PlayerPrefs.GetInt("Best Score", finalScore))
+        finalScore = PlayerPrefs.GetInt("Best Score", finalScore);
+        if (highScore < finalScore)
+            submitButton.gameObject.SetActive(true);
             PlayerPrefs.SetInt("Best Score", finalScore);
-            HighScores.UploadScore(myName.text, PlayerPrefs.GetInt("Best Score", finalScore));
+            HighScores.UploadScore(myName.text, finalScore);
+    }
+    public void Submit()
+    {
+        enterName.text = " "+ myName;
     }
 }
